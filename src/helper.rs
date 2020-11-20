7a4d60fe77b893094086ca5c9f374e0753df87e1
lightsing <light.tsing@gmail.com>
2020-11-20 10:58:30 +0800

    port helper

---
+++ src/helper.rs
@@ -0,0 +1,109 @@
+use std::sync::Arc;
+use bytes::{Bytes, BytesMut};
+use futures::Stream;
+use futures::task::Context;
+use serde::Deserialize;
+use tokio::macros::support::{Pin, Poll};
+use tokio::sync::{watch, mpsc, RwLock};
+use tokio::stream::StreamExt;
+use actix_http::Response;
+use actix_http::body::Body;
+
+use crate::error::Error;
+use crate::ACTIVE_DOWNLOADS;
+
+#[derive(Clone, Debug, Deserialize, Hash, Eq, PartialEq)]
+pub struct CrateReq {
+    #[serde(rename = "crate")]
+    name: String,
+    version: String,
+}
+
+lazy_static! {
+    static ref CLIENT: reqwest::Client = reqwest::Client::new();
+}
+
+#[derive(Clone, Debug)]
+pub struct Crate {
+    name: String,
+    version: String,
+    content_type: String,
+    content_length: usize,
+    pub buffer: Arc<RwLock<BytesMut>>,
+    pub notify: watch::Receiver<usize>,
+    ptr: usize,
+}
+
+impl Crate {
+    pub async fn create(krate_req: CrateReq) -> Result<Arc<Self>, Error> {
+        if let Some(krate) = ACTIVE_DOWNLOADS.read().await.get(&krate_req) {
+            return Ok(krate.clone())
+        }
+        let mut guard = ACTIVE_DOWNLOADS.write().await;
+        let CrateReq { name, version } = krate_req.clone();
+        let uri = format!(
+            "https://static.crates.io/crates/{name}/{name}-{version}.crate",
+            name = name,
+            version = version
+        );
+        let resp = CLIENT.get(&uri).send().await?;
+        let content_length = resp.content_length().ok_or_else(|| Error::MissingField)? as usize;
+        let (tx, rx) = watch::channel(0);
+        let krate = Self {
+            name,
+            version,
+            content_type: resp.headers().get("content-type").ok_or_else(|| Error::MissingField)?.to_str()?.to_string(),
+            content_length,
+            buffer: Arc::new(RwLock::new(BytesMut::with_capacity(content_length as usize))),
+            notify: rx,
+            ptr: 0
+        };
+        let write_buffer = krate.buffer.clone();
+        tokio::spawn(async move {
+            let mut stream = resp.bytes_stream();
+            while let Some(chunk) = stream.next().await {
+                match chunk {
+                    Ok(data) => {
+                        let mut buffer = write_buffer.write().await;
+                        buffer.extend_from_slice(&data[..]);
+                        tx.broadcast(data.len()).unwrap();
+                    }
+                    Err(e) => {
+                        error!("{}", e);
+                        break;
+                    }
+                };
+            }
+            // TODO: Upload to upyun
+        });
+        guard.insert(krate_req.clone(), Arc::new(krate));
+        return Ok(guard.get(&krate_req).unwrap().clone())
+    }
+
+    pub fn tee(&self, tx: mpsc::UnboundedSender<Result<Bytes, ()>>) {
+        let mut notify = self.notify.clone();
+        let krate = self.clone();
+        tokio::spawn(async move {
+            let mut ptr = 0;
+            loop {
+                let data = {
+                    let buffer = krate.buffer.read().await;
+                    let data = Bytes::copy_from_slice(&buffer[ptr..]);
+                    ptr += data.len();
+                    data
+                };
+                match tx.send(Ok(data)) {
+                    Ok(_) => (),
+                    Err(e) => {
+                        error!("{}", e);
+                        break;
+                    }
+                }
+                info!("{}/{}", ptr, krate.content_length);
+                if ptr == krate.content_length || notify.recv().await.is_none() {
+                    break;
+                }
+            }
+        });
+    }
+}
