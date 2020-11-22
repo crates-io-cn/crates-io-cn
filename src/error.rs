ff2be72736969431c9f61f198c1372765ca8100a
lightsing <light.tsing@gmail.com>
2020-11-22 16:21:07 +0800

    more error

--- src/error.rs
+++ src/error.rs
@@ -2,8 +2,13 @@ use derive_more::{Display, Error, From};
 
 #[derive(Debug, Display, From, Error)]
 pub enum Error {
+    IO(std::io::Error),
     Reqwest(reqwest::Error),
     Header(reqwest::header::ToStrError),
+    SerdeJSON(serde_json::Error),
+    Git2(git2::Error),
+    EasyGit(easy_git::Error),
     MissingField,
     FetchFail,
+    InvalidUTF8,
 }
