a46d5b06365d8f3918b546eccfb907b39b35a251
lightsing <light.tsing@gmail.com>
2021-01-14 13:30:48 +0800

    more log

--- src/systemd.rs
+++ src/systemd.rs
@@ -4,13 +4,15 @@ use systemd::daemon::{
 };
 
 pub fn notify_ready() {
-    if notify(false, [(STATE_READY, "1")].iter()).is_err() {
-        warn!("fail to notify systemd (ready)")
+    match notify(false, [(STATE_READY, "1")].iter()) {
+        Ok(result) => info!("notify result: {}", result),
+        Err(e) => error!("fail to notify ready: {}", e)
     }
 }
 
 pub fn notify_watchdog() {
-    if notify(false, [(STATE_WATCHDOG, "1")].iter()).is_err() {
-        warn!("fail to poke watchdog");
+    match notify(false, [(STATE_WATCHDOG, "1")].iter()) {
+        Ok(result) => info!("poke watchdog result: {}", result),
+        Err(e) => error!("fail to poke watchdog: {}", e)
     }
 }
